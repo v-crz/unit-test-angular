7cf2cd61d7863db92a64230c989286f2988d34bd
VC <vbatten10@hotmail.com>
2022-03-31 22:02:44 -0600

    Add: Second option to test router

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,30 +1,40 @@
 import { NavComponent } from './nav.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 
 // Clase fake para no importar todo lo que necesitan los componentes Home y Cart
-class ComponentTestRoute {};
+// class ComponentTestRoute {};
 
-describe('Nav component', () => {
+const routerMock = {
+    navigate() {}
+};
+
+fdescribe('Nav component', () => {
     let component: NavComponent;
     let fixture: ComponentFixture<NavComponent>;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
-                RouterTestingModule.withRoutes([
-                    // { path: 'home', component: HomeComponent },
-                    // { path: 'cart', component: CartComponent }
-                    { path: 'home', component: ComponentTestRoute },
-                    { path: 'cart', component: ComponentTestRoute }
-                ])
+                // RouterTestingModule.withRoutes([
+                //     // { path: 'home', component: HomeComponent },
+                //     // { path: 'cart', component: CartComponent }
+                //     { path: 'home', component: ComponentTestRoute },
+                //     { path: 'cart', component: ComponentTestRoute }
+                // ])
             ],
             declarations: [
                 NavComponent
             ],
-            providers: [],
+            providers: [
+                // Router va a ser routerMock
+                {
+                    provide: Router,
+                    useValue: routerMock
+                }
+            ],
             schemas: [
                 CUSTOM_ELEMENTS_SCHEMA,
                 NO_ERRORS_SCHEMA
@@ -42,17 +52,28 @@ describe('Nav component', () => {
         expect(component).toBeTruthy();
     });
     
+    // it('Should navigate', () => {
+    //     // Obtener router
+    //     const router = TestBed.inject(Router);
+
+    //     // Espiar navigate
+    //     const spy = spyOn(router, 'navigate');
+
+    //     component.navTo('home');
+    //     expect(spy).toHaveBeenCalledWith(['/home']);
+
+    //     component.navTo('cart');
+    //     expect(spy).toHaveBeenCalledWith(['/cart']);
+    // });
+
     it('Should navigate', () => {
-        // Obtener router
+        // Obtener router del provider
         const router = TestBed.inject(Router);
 
         // Espiar navigate
         const spy = spyOn(router, 'navigate');
 
-        component.navTo('home');
-        expect(spy).toHaveBeenCalledWith(['/home']);
-
-        component.navTo('cart');
-        expect(spy).toHaveBeenCalledWith(['/cart']);
+        component.navTo('');
+        expect(spy).toHaveBeenCalled();
     });
 });
