830cb0c5b8311610b4767fc2b29e4cd8f70d6d10
VC <vbatten10@hotmail.com>
2022-03-31 21:49:40 -0600

    Add: Testing navigate with spy

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -2,6 +2,7 @@ import { NavComponent } from './nav.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 // Clase fake para no importar todo lo que necesitan los componentes Home y Cart
 class ComponentTestRoute {};
@@ -37,7 +38,21 @@ describe('Nav component', () => {
         fixture.detectChanges();
     });
 
-    it('Shoud create', () => {
+    it('Should create', () => {
         expect(component).toBeTruthy();
+    });
+    
+    it('Should navigate', () => {
+        // Obtener router
+        const router = TestBed.inject(Router);
+
+        // Espiar navigate
+        const spy = spyOn(router, 'navigate');
+
+        component.navTo('home');
+        expect(spy).toHaveBeenCalledWith(['/home']);
+
+        component.navTo('cart');
+        expect(spy).toHaveBeenCalledWith(['/cart']);
     });
 });
